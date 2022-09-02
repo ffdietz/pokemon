847b53533df727c368d819f2f19f409e072ade4a
ffdietz <felipefierrod@gmail.com>
2022-09-02 11:31:31 +0200

    index links styling

--- src/components/PokemonIndex.jsx
+++ src/components/PokemonIndex.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
-import { Box, Input, Text } from '@chakra-ui/react'
+import { Box, Input, Flex, Link } from '@chakra-ui/react'
 
 const PokemonIndex = ({ pokemons }) => {
   const [filter, setFilter] = useState('');
@@ -12,20 +12,21 @@ const PokemonIndex = ({ pokemons }) => {
   return (
     <Box width='35%' height='100%' borderWidth='1px' borderRadius='lg' overflow='scroll' overflowX='hidden'>
       
-      <Input onChange={handleFilter} placeholder='search...' size='lg' width='30%' fontSize='1.5rem' color='black'
-        position='fixed' background='#FFF'/>
-      <Box>
+      <Input onChange={handleFilter} placeholder='search...' width='30%' height='4rem' fontSize='1.5rem' color='black'
+        position='fixed' background='yellow.100' margin='0'/>
+      <Box marginTop='5rem'>
         { pokemons &&
           pokemons.map((pokemon, key) => {
             return (
               pokemon.name.includes(filter)&&
-              <div key={key}>
-                <NavLink to={`/pokemon/${pokemon.name}`} >
-                  <Text fontSize='2.5rem' color='grey' borderWidth='1px' borderRadius='5' padding='0.3em'>
+              <Flex key={key} border='1px' borderRadius='5' color='grey' padding='0.7rem'>
+                  <Link as={NavLink} to={`/pokemon/${pokemon.name}`} _activeLink={{ color: 'red' }}
+                    fontSize='2.5rem' _hover={{ color: "teal.500" }} 
+                  >
                     {pokemon.name}
-                  </Text>
-                </NavLink>
-              </div>
+                  {/* </Text> */}
+                </Link>
+              </Flex>
             )
           })
         }
